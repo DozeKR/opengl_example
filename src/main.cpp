d86a196007eff37155ff169e61895f42c6bfe132
Doze <jd05183@naver.com>
2021-03-19 16:44:47 +0900

    Second Commit

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,8 @@
-#include<spdlog/spdlog.h>
-#include<glad/glad.h>
-#include<GLFW/glfw3.h>
+#include "context.h"
+
+#include <spdlog/spdlog.h>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
 
 void OnFramebufferSizeChange(GLFWwindow* window, int width, int height) {
     SPDLOG_INFO("framebuffer size changed: ({} x {})", width, height);
@@ -51,12 +53,19 @@ int main(int argc, const char** argv)
 
     // glad를 활용한 OpenGL 함수 로딩
     if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
-    SPDLOG_ERROR("failed to initialize glad");
-    glfwTerminate();
-    return -1;
+        SPDLOG_ERROR("failed to initialize glad");
+        glfwTerminate();
+        return -1;
     }
     auto glVersion = glGetString(GL_VERSION);
     SPDLOG_INFO("OpenGL context version: {}", glVersion);
+
+    auto context = Context::Create();
+    if (!context) {
+        SPDLOG_ERROR("failed to create context");
+        glfwTerminate();
+        return -1;
+    }
 
     OnFramebufferSizeChange(window, WINDOW_WIDTH, WINDOW_HEIGHT);
     glfwSetFramebufferSizeCallback(window, OnFramebufferSizeChange);
@@ -66,10 +75,10 @@ int main(int argc, const char** argv)
     SPDLOG_INFO("Start main loop");
     while (!glfwWindowShouldClose(window)) {
         glfwPollEvents();
-        glClearColor(0.5f, 0.1f, 0.0f, 0.0f);
-        glClear(GL_COLOR_BUFFER_BIT);
+        context->Render();
         glfwSwapBuffers(window);
     }
+    context.reset();    //다른 방법 context = nullptr;
 
     glfwTerminate();
     return 0;
